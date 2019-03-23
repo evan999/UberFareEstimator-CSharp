44ca10d39750e11259082322587397c6aef110cb
evan999 <emeshberge@gmail.com>
2019-03-23 10:25:45 -0400

    Object-oriented local solution of CodeSignal problem. Working on touching it up some more.

--- UberFareEstimator/Program.cs
+++ UberFareEstimator/Program.cs
@@ -10,6 +10,18 @@ namespace UberFareEstimator
     {
         static void Main(string[] args)
         {
-        }
+            Ride trip = new Ride();
+            double[] minute_cost = { 0.2, 0.35, 0.4, 0.45 };
+            double[] mile_cost = { 1.1, 1.8, 2.3, 3.5 };
+            double[] results = trip.FareEstimator(30, 7, minute_cost, mile_cost);
+
+            foreach(var result in results)
+            {
+                Console.WriteLine(result.ToString());
+            }
+            
+
+            Console.ReadLine();
+    }
     }
 }
---
+++ UberFareEstimator/Ride.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberFareEstimator
+{
+    public class Ride
+    {
+        /*
+        public int time;
+        public int distance;
+
+        public double[] 
+
+        
+        // public int distance;
+
+        public Ride(int time, int distance)
+        {
+            this.time = time;
+            this.distance = distance;
+        }
+        */
+
+        
+        
+        /*
+        public int time
+        {
+
+            get
+            {
+                return time;
+            }
+
+            set
+            {
+                time = value;
+            }
+        }
+        */
+
+        /*
+        public int distance
+        {
+            get
+            {
+                return distance;
+            }
+
+            set
+            {
+                distance = value;
+            }
+        }
+        */
+
+        // public double[] costPerMinute;
+        // public double[] costPerMile;
+
+        
+
+        public double[] FareEstimator(int ride_time, int ride_distance, double[] cost_per_minute, double[] cost_per_mile)
+        {
+            // (Cost per minute) * (ride time) + (Cost per mile) * (ride distance)
+            // Problem specification:
+            // Return the fare estimates for all car types
+            // How much will the ride cost for each car type?
+
+            double[] fareEstimate = new double[cost_per_mile.Length];
+
+            for (int i = 0; i < cost_per_mile.Length; i++)
+            {
+                // mileCost = cost_per_mile[i];
+                // minuteCost = cost_per_minute[i];
+                fareEstimate[i] = (double)(cost_per_minute[i] * ride_time) + (double)(cost_per_mile[i] * ride_distance);
+            }
+
+            // Return result array
+            return fareEstimate;
+        }
+
+
+    }
+}
